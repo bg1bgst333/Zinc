8163dce24eaf1c9005c6ad09082668aa6f2f06fe
BG1 <bg1bgst333@gmail.com>
2016-10-11 13:16:22 +0900

    ・URLのロード

--- android/empty_activity/android_studio/src/Zinc/Zinc/app/src/main/java/com/bgstation0/android/application/zinc/MainActivity.java
+++ android/empty_activity/android_studio/src/Zinc/Zinc/app/src/main/java/com/bgstation0/android/application/zinc/MainActivity.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
     // メンバフィールドの定義
     private FragmentManager fragmentManager = null; // FragmentManager型fragmentManagerをnullにセット.
     private final String FRAGMENT_TAB_PREFIX_WEB = "web";   // 定数FRAGMENT_TAG_PREFIX_WEBを"web"とする.
+    private final String BUNDLE_ARGUMENT_KEY_URL = "url";   // 定数BUNDLE_ARGUMENT_KEY_URLを"url"とする.
+    private final String HTTP_URL_BG1_BLOG = "http://bg1.hatenablog.com";   // 定数HTTP_URL_BG1_BLOGを"http://bg1.hatenablog.com"とする.
     private int webFragmentNo = 0;  // int型webFragmentNoを0にセット.
+    private String currentFragmentTag = null;   // String型currentFragmentTagをnullにセット.
 
+    // アクティビティの生成時.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +27,20 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();   // fragmentManager.beginTransactionでfragmentTransaction取得.
         String fragmentTag = FRAGMENT_TAB_PREFIX_WEB + webFragmentNo;   // fragmentTagはFRAGMENT_TAG_PREFIX_WEBにwebFragmentNoを付けたものとする.
         WebFragment webFragment = new WebFragment();    // WebFragmentオブジェクトwebFragmentの生成.
+        Bundle args = new Bundle(); // Bundle型argsを生成.
+        args.putString(BUNDLE_ARGUMENT_KEY_URL, HTTP_URL_BG1_BLOG); // args.putStringでBUNDLE_ARGUMENT_KEY_URLをキー, HTTP_URL_BG1_BLOGを値として登録.
+        webFragment.setArguments(args); // webFragment.setArgumentsでargsを渡す.
         fragmentTransaction.add(R.id.content, webFragment, fragmentTag);    // fragmentTransaction.addでwebFragmentをfragmentTagを付けてR.id.contentに追加.
         fragmentTransaction.commit();   // fragmentTransaction.commitで確定.
+        currentFragmentTag = fragmentTag;   // currentFragmentTagにfragmentTagを格納.
         webFragmentNo++;    // webFragmentNoをインクリメント.
+        //loadUrl("http://bg1.hatenablog.com");  // loadUrlで"http://bg1.hatenablog.com"をロード.(これだとfragmentManager.findFragmentByTagでみつからず落ちる.)
     }
 
+    // URLのロード.
+    public void loadUrl(String url){
+        // 現在のフラグメントを取得して, そのフラグメントでロードする.
+        WebFragment webFragment = (WebFragment)fragmentManager.findFragmentByTag(currentFragmentTag);   // currentFragmentTagでwebFragmentを引く.
+        webFragment.loadUrl(url);   // webFragment.loadUrlでurlをロード.
+    }
 }
--- android/empty_activity/android_studio/src/Zinc/Zinc/app/src/main/java/com/bgstation0/android/application/zinc/WebFragment.java
+++ android/empty_activity/android_studio/src/Zinc/Zinc/app/src/main/java/com/bgstation0/android/application/zinc/WebFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 
 /**
@@ -13,17 +14,39 @@ import android.view.ViewGroup;
  */
 public class WebFragment extends Fragment {
 
+    // メンバフィールドの定義
+    private View fragmentView = null;   // View型fragmentViewをnullにセット.
+    private WebView webView = null; // WebView型webViewをnullにセット.
+    private final String BUNDLE_ARGUMENT_KEY_URL = "url";   // 定数BUNDLE_ARGUMENT_KEY_URLを"url"とする.
 
+    // コンストラクタ
     public WebFragment() {
         // Required empty public constructor
     }
 
-
+    // ビューの生成時.
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web, container, false);
+        // ビューの生成.
+        fragmentView = inflater.inflate(R.layout.fragment_web, container, false);   // inflater.inflateでR.layout.fragment_webをベースにfragmentViewを生成.
+        // 引数の受領.
+        Bundle args = getArguments();   // getArgumentsでargs取得.
+        if (args != null) {
+            String url = args.getString(BUNDLE_ARGUMENT_KEY_URL);   // args.getStringでurlを取得.
+            if (url != null){
+                loadUrl(url);   // loadUrlでurlをロード.
+            }
+        }
+        return fragmentView;    // fragmentViewを返す.
+    }
+
+    // URLのロード.
+    public void loadUrl(String url){
+        // webViewを探してロードさせる.
+        webView = (WebView)fragmentView.findViewById(R.id.webView);  // webViewを取得.
+        webView.loadUrl(url);   // webView.loadUrlでロード.
     }
 
 }

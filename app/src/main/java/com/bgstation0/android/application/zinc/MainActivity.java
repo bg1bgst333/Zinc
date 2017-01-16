b8da039849d2e39526f50a400a4aa394427515ef
BG1 <bg1bgst333@gmail.com>
2017-01-16 18:28:13 +0900

    ・WebViewの設置

--- app/src/main/java/com/bgstation0/android/application/zinc/MainActivity.java
+++ app/src/main/java/com/bgstation0/android/application/zinc/MainActivity.java
@@ -2,6 +2,7 @@ package com.bgstation0.android.application.zinc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // WebViewオブジェクトwebViewを取得し, loadUrlで指定のURLをロード.
+        WebView webView = (WebView)findViewById(R.id.webView);  // findViewByIdでR.id.webViewなwebViewを探して取得.
+        webView.loadUrl("http://www.google.co.jp"); // loadUrlで"http://www.google.co.jp"をロード.
     }
 }

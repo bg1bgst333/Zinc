07d88e34e2f90f28b3d666d10f495c2a11b64d8d
BG1 <bg1bgst333@gmail.com>
2017-01-16 12:11:04 +0900

    clean Zinc/develop

--- android/empty_activity/eclipse_adt/src/Zinc/Zinc/gen/com/bgstation0/android/application/zinc/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.bgstation0.android.application.zinc;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
